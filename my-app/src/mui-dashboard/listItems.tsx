71d926ad8fe441a7137523c4cee10cb65d5a925a
Seth Denburg <Denburg.Seth@gmail.com>
2024-04-04 20:51:27 -0400

    Added nav links to pages

--- my-app/src/mui-dashboard/listItems.tsx
+++ my-app/src/mui-dashboard/listItems.tsx
@@ -8,21 +8,26 @@ import PeopleIcon from '@mui/icons-material/People';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import LayersIcon from '@mui/icons-material/Layers';
 import AssignmentIcon from '@mui/icons-material/Assignment';
+import Link from 'next/link';
 
 export const mainListItems = (
   <React.Fragment>
-    <ListItemButton>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary="Search" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Jobs" />
-    </ListItemButton>
+    <Link href={'/search'}>
+      <ListItemButton>
+        <ListItemIcon>
+          <LayersIcon />
+        </ListItemIcon>
+        <ListItemText primary="Search" />
+      </ListItemButton>
+    </Link>
+    <Link href={'/results'}>
+      <ListItemButton>
+        <ListItemIcon>
+          <AssignmentIcon />
+        </ListItemIcon>
+        <ListItemText primary="Jobs" />
+      </ListItemButton>
+    </Link>
     <ListItemButton>
       <ListItemIcon>
         <BarChartIcon />
